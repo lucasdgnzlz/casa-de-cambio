61f5e0ff7450b0289b63966806a86b27168b596c
Lucas <lucasdgonzalez@outlook.com>
2023-07-27 20:31:07 -0300

    Agrega pruebas con cypress
    Añade pruebas de visibilidad con cypress.

---
+++ cypress/e2e/casa-de-cambio.cy.js
@@ -0,0 +1,33 @@
+const URL = "http://192.168.1.41:8080/docs/";
+
+context("Casa De Cambio", () => {
+  beforeEach(() => {
+    cy.visit(URL);
+  });
+
+  describe("Comprueba visibilidad de elementos", () => {
+    it("Comprueba que la cabecera con el título sean visibles", () => {
+      cy.get("header").should("be.visible");
+    });
+
+    it("Comprueba que la sección de inicio sea visible", () => {
+      cy.get(".inicio").should("be.visible");
+    });
+
+    it("Comprueba que la sección de 'sobre nosotros' sea visible", () => {
+      cy.get(".sobre-nosotros").should("be.visible");
+    });
+
+    it("Comprueba que la sección de conversión de divisas sea visible", () => {
+      cy.get(".conversion-divisas").should("be.visible");
+    });
+
+    it("Comprueba que la sección de cambios de divisas sea visible", () => {
+      cy.get(".cambios-de-divisas").should("be.visible");
+    });
+
+    it("Comprueba que el pie de la página sea visible", () => {
+      cy.get("footer").should("be.visible");
+    });
+  });
+});
